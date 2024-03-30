d7cb6e8d1362a4041e531a234770128e8ea2893d
Osilaja78 <osilajaabdulhameed@gmail.com>
2024-03-30 18:21:12 +0100

    fixed suspense error

--- frontend/app/auth/verify_token/page.jsx
+++ frontend/app/auth/verify_token/page.jsx
@@ -1,64 +1,68 @@
-"use client"
-import { useState, useEffect, Suspense } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-import { toast, ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-import { warn, notify } from "@/app/layout";
-import { baseApiUrl } from "@/app/layout";
+// "use client"
+// import { useState, useEffect, Suspense } from "react";
+// import { useRouter, useSearchParams } from "next/navigation";
+// import { toast, ToastContainer } from "react-toastify";
+// import 'react-toastify/dist/ReactToastify.css';
+// import { warn, notify } from "@/app/layout";
+// import { baseApiUrl } from "@/app/layout";
 
 
-export default function VerifyToken() {
+// export default function VerifyToken() {
 
-    const [response, setResponse] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState('');
-    const searchParams = useSearchParams();
-    const router = useRouter();
+//     const [response, setResponse] = useState(null);
+//     const [loading, setLoading] = useState(false);
+//     const [error, setError] = useState('');
+//     const searchParams = useSearchParams();
+//     const router = useRouter();
     
-    useEffect(() => {
-        const verifyToken = async (e) => {
-            setLoading(true)
+//     useEffect(() => {
+//         const verifyToken = async (e) => {
+//             setLoading(true)
 
-            try {
-                const res = await fetch(`${baseApiUrl}/auth/verify-token?token=${searchParams.get('token')}`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded',
-                    },
-                });
+//             try {
+//                 const res = await fetch(`${baseApiUrl}/auth/verify-token?token=${searchParams.get('token')}`, {
+//                     method: 'GET',
+//                     headers: {
+//                         'Content-Type': 'application/x-www-form-urlencoded',
+//                     },
+//                 });
 
-                if (res.ok) {
-                    console.log(res);
-                    setResponse(res.data.message);
-                    setLoading(false);
-                    setTimeout(() => {
-                        router.push("/auth/signin")
-                    }, 2000);
-                } else {
-                    console.log(res);
-                }
-            } catch (err) {
-                setError(err.response.data.detail);
-                setLoading(false);
-            }
-        }
-        verifyToken();
-    }, []);
+//                 if (res.ok) {
+//                     console.log(res);
+//                     setResponse(res.data.message);
+//                     setLoading(false);
+//                     setTimeout(() => {
+//                         router.push("/auth/signin")
+//                     }, 2000);
+//                 } else {
+//                     console.log(res);
+//                 }
+//             } catch (err) {
+//                 setError(err.response.data.detail);
+//                 setLoading(false);
+//             }
+//         }
+//         verifyToken();
+//     }, []);
 
-    if (error) {
-		warn(`${error}`);
-	}
+//     if (error) {
+// 		warn(`${error}`);
+// 	}
 
-	if (response) {
-		notify(`${response}`)
-	}
+// 	if (response) {
+// 		notify(`${response}`)
+// 	}
 
-    return (
-        <Suspense fallback={<p>Loading...</p>}>
-            <section className="h-[100vh]">
-                {loading ? <p className="text-[50px] text-center">Loading...</p> : ''}
-                <ToastContainer />
-            </section>
-        </Suspense>
-    )
+//     return (
+//         <Suspense fallback={<p>Loading...</p>}>
+//             <section className="h-[100vh]">
+//                 {loading ? <p className="text-[50px] text-center">Loading...</p> : ''}
+//                 <ToastContainer />
+//             </section>
+//         </Suspense>
+//     )
+// }
+
+const nothingHere= () => {
+    return "Nothing to see here";
 }
